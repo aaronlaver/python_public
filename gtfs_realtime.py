7d552a33f471f22c455436734c74d86686c073e9
aaronlaver <64020020+aaronlaver@users.noreply.github.com>
2021-09-02 21:09:24 -0400

    Update gtfs_realtime.py

--- gtfs_realtime.py
+++ gtfs_realtime.py
@@ -4,7 +4,9 @@ import urllib
 feed = gtfs_realtime_pb2.FeedMessage()
 response = urllib.request.urlopen('http://realtime.cota.com/TMGTFSRealTimeWebService/Vehicle/VehiclePositions.pb')
 feed.ParseFromString(response.read())
+print(feed.entity) # prints entire feed. Code after this prints each vehicle in feed
+"""
 for entity in feed.entity:
     if entity.HasField('trip_update'):
         print(entity.trip_update)
-    print(entity)
+"""
